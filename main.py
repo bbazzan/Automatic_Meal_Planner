62a4e0127e2513e36ab5e036b1a1dcfcbfb5ac6a
bbazzan <60471166+bbazzan@users.noreply.github.com>
2022-03-29 18:39:53 -0300

    minor modifications

--- main.py
+++ main.py
@@ -6,3 +6,6 @@ from googleapiclient.discovery import build
 from google.oauth2 import service_account
 import config as cfg
 
+creds = service_account.Credentials.from_service_account_file('automatic-meal-planner-345619-c7654b74a820.json', scopes=cfg.SCOPES)
+
+
