ca87818d0166eeeef4946511cc943f16416630c3
bbazzan <60471166+bbazzan@users.noreply.github.com>
2022-03-29 17:12:52 -0300

    created the main.py file and the ignored config.py file

---
+++ main.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+from pathlib import Path
+from datetime import datetime, timedelta
+from googleapiclient.discovery import build
+from google.oauth2 import service_account
+import config as cfg
+
